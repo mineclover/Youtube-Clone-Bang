8a38a95808ff328cad0f6b832e66a9893ef519ab
mineclover <mineclover@naver.com>
2023-01-16 16:44:15 +0900

    env. folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.scss';
+import Header from './components/header';
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
 }
 
 export default App;
